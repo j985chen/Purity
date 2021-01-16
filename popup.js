ba7509bc812e6a7564eee96dba93b1858a20cd5d
Janet Chen <j985chen@uwaterloo.ca>
2021-01-16 16:35:23 -0500

    quick fix for facebook secure authentication issue

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ const button = document.getElementById("popup_button");
 
 button.addEventListener("click", () => {
     console.log("clicked");
-    chrome.tabs.create({ url: "http://childproof.herokuapp.com/" });
+    chrome.tabs.create({ url: "https://childproof.herokuapp.com/signIn" });
 });
 
 var passwordProperty;
