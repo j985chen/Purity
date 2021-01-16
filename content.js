1317b769c20ca6ce66e34c4bb2bc9fd589505dba
Iva Chho <ichho20@pcstudents.us>
2021-01-16 15:50:42 -0700

    filtering/replacing words

--- content.js
+++ content.js
@@ -408,8 +408,182 @@ function toggleFilter(){
 loadSettings();
 
 //-------much simpler script to replace words --------// 
-/*
+
 var elements = document.getElementsByTagName('*');
+
+var sourceWordsToTargetWords = [
+	[['anal'], 'butt'],
+	[['ass-hat'], 'butt-hat'],
+	[['assbag'], 'buttbag'],
+	[['assbite'], 'buttbite'],
+	[['asscock'], 'buttcrook'],
+	[['asses'], 'butts'],
+	[['assfuck'], 'buttmate'],
+	[['asshead'], 'butthead'],
+	[['asshole'], 'butthole'],
+	[['asslick'], 'buttlick'],
+	[['asslicker'], 'buttlicker'],
+	[['asslicker'], 'buttlicker'],
+	[['assmonkey'], 'buttmonkey'],
+	[['assmunch'], 'buttmunch'],
+	[['assshit'], 'butard'],
+	[['asssucker'], 'buttsucker'],
+	[['bastard'], 'no father'],
+	[['bitch'], 'b****'],
+	[['bitchass'], 'b****a**'],
+	[['bitchy'], 'rude'],
+	[['blowjob'], '*******'],
+	[['bullshit'], 'bull crap'],
+	[['clitface'], 'face'],
+	[['clusterfuck'], 'mess'],
+	[['cock'], 'rooster'],
+	[['cockass'], 'nonsense'],
+	[['cockbite'], 'c***bite'],
+	[['cockburger'], 'c***burger'],
+	[['cockface'], 'c***face'],
+	[['cockhead'], 'c***head'],
+	[['cockmonkey'], 'monkey'],
+	[['cocknose'], 'c***nose'],
+	[['cocknugget'], 'chicken nugget'],
+	[['cockshit'], 'c***s***'],
+	[['cockwaffle'], 'waffle'],
+	[['creampie'], 'food'],
+	[['cum'], 'c**'],
+	[['cumbubble'], 'c**bubble'],
+	[['cumdumpster'], 'c**dumpster'],
+	[['cumslut'], 'c**s***'],
+	[['cumtart'], 'c**tart'],
+	[['cunt'], 'c***'],
+	[['cuntass'], 'irritating'],
+	[['cuntface'], 'irritating'],
+	[['cuntrag'], 'irritating'],
+	[['cuntslut'], 'annoying'],
+	[['damn'], 'dang'],
+	[['dammit'], 'dang it'],
+	[['deepthroat'], 'throat'],
+	[['dick'], 'd***'],
+	[['dickbag'], 'd***bag'],
+	[['dickface'], 'd***face'],
+	[['dickfuck'], 'd***f***'],
+	[['dickfucker'], 'd***f*****'],
+	[['dickhead'], 'd***head'],
+	[['dickjuice'], 'juice'],
+	[['dickmilk'], 'milk'],
+	[['dicksucker'], 'd***sucker'],
+	[['dickwad'], 'mean'],
+	[['dickweasel'], 'mean'],
+	[['dickweed'], 'mean'],
+	[['dickwod'], 'rude'],
+	[['dildo'], 'toy'],
+	[['dildos'], 'toys'],
+	[['dipshit'], 'loser'],
+	[['doochbag'], 'duck'],
+	[['douche'], 'duck'],
+	[['douchebag'], 'duck'],
+	[['douchefag'], 'duck'],
+	[['dumbass'], 'dummy'],
+	[['dumb ass'], 'dummy'],
+	[['dumass'], 'dummy'],
+	[['dumbfuck'], 'dummy'],
+	[['dumbshit'], 'dummy'],
+	[['dumshit'], 'dummy'],
+	[['fag'], 'loser'],
+	[['faggot'], 'loser'],
+	[['fagfucker'], 'boyfriend'],
+	[['fuck'], 'frick'],
+	[['fuckable'], 'frickable'],
+	[['fucking'], 'freaking'],
+	[['fucked'], 'fricked'],
+	[['fuckass'], 'frick'],
+	[['fucker'], 'fricker'],
+	[['fuckers'], 'frickers'],
+	[['fuckin'], 'freakin'],
+	[['handjob'], 'h******'],
+	[['jizz'], 'j***'],
+	[['motherfucker'], 'motherfricker'],
+	[['orgy'], 'group'],
+	[['piss'], 'pee'],
+	[['pissed'], 'mad'],
+	[['pissing'], 'urinating'],
+	[['shit'], 's***'],
+	[['shite'], 's***'],
+	[['shitting'], 'pooping'],
+	[['shite'], 'poop'],
+	[['thefuck'], 'the heck'],
+	[['whore'], 'w****'],
+	[['whores'], 'w*****'],
+	[['slut'], 's***'],
+	[['slutty'], 's*****'],
+	[['hoe'], 'h**'],
+	[['ho'], 'h*'],
+	[['ballots'], 'votes'],
+	[['ballot'], 'vote'],
+	[['discord'], 'disagreement'],
+	[['statehouse'], 'government building'],
+	[['additional'], 'more'],
+	[['bracing'], 'getting ready for'],
+	[['confrontation'], 'fight'],
+	[['object to'], 'disagree with'],
+	[['object'], 'disagree'],
+	[['perpetuating'], 'maintaining'],
+	[['perpetuate'], 'maintain'],
+	[['gearing up'], 'getting ready'],
+	[['nation'], 'country'],
+	[['domestic extremists'], 'terrorists'],
+	[['domestic extremist'], 'terrorist'],
+	[['prior to'], 'before'],
+	[['amend'], 'change'],
+	[['federal'], 'government'],
+	[['reprehensible'], 'wrong'],
+	[['numerous'], 'a lot'],
+	[['received'], 'gotten'],
+	[['receive'], 'get'],
+	[['legitimize'], 'confirm'],
+	[['unsupported'], 'wrong'],
+	[['attended'], 'went to'],
+	[['attend'], 'go to'],
+	[['falsehoods'], 'lies'],
+	[['falsehood'], 'lie'],
+	[['integrity'], 'quality'],
+	[['law enforcement'], 'police'],
+
+];
+
+function makeRegex(sourceWords) {
+    return new RegExp('\\b' + sourceWords.join('\\b|\\b') + '\\b', 'g');
+};
+
+function identity(string) {
+    return string;
+};
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+function toUpperCase(string) {
+    return string.toUpperCase();
+};
+
+function makeRegexToTargetWords(sourceWordsToTargetWords, modifyWords) {
+    return sourceWordsToTargetWords.map(function(sourceAndTarget) {
+        var [source,target] = sourceAndTarget;
+        source = source.map(modifyWords);
+        target = modifyWords(target);
+        return [makeRegex(source), target];
+    });
+};
+
+var sourceRegexToTargetWords = makeRegexToTargetWords(sourceWordsToTargetWords, identity).concat(makeRegexToTargetWords(sourceWordsToTargetWords, capitalizeFirstLetter)).concat(makeRegexToTargetWords(sourceWordsToTargetWords, toUpperCase));
+
+function replaceTextWithRegexes(text, sourceRegexToTargetWords) {
+    for (var k = 0; k < sourceRegexToTargetWords.length; k++) {
+        var [regex, targetWord] = sourceRegexToTargetWords[k];
+        var replacedText = text.replace(regex, targetWord);
+        text = replacedText
+    }
+    return text;
+};
 
 for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
@@ -419,11 +593,12 @@ for (var i = 0; i < elements.length; i++) {
 
         if (node.nodeType === 3) {
             var text = node.nodeValue;
-			var replacedText = text.replace(/[word to replace]/gi, '[new word or phrase]');
+            replacedText = replaceTextWithRegexes(text, sourceRegexToTargetWords);
 
             if (replacedText !== text) {
+                console.log('replaced');
                 element.replaceChild(document.createTextNode(replacedText), node);
             }
         }
     }
-*/
+}
